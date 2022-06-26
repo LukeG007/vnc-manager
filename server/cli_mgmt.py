478cf30083160028e61bb386cc672f144a0735fc
LukeG007 <luke@gilsinger.net>
2022-06-26 15:36:01 -0400

    Add to help command

--- server/cli_mgmt.py
+++ server/cli_mgmt.py
@@ -5,14 +5,19 @@ class CLI:
     def __init__(self):
         self.auth = auth_cli.AuthenticationManagement()
         self.cmd_callbacks = {
-            'help': {'usage': 'Display Command usages', 'callback': self.help},
+            'help': {'usage': 'Display Command Details', 'callback': self.help},
             
         }
     def help(self):
-        help_msg = ''
+        help_msg = 'Command\tDescription\n'
+        x = 1
         for cmd in self.cmd_callbacks:
             usage = self.cmd_callbacks[cmd]['usage']
-            help_msg += cmd + '\t' + usage + '\n'
+            if not x == len(self.cmd_callbacks):
+                help_msg += cmd + '\t' + usage + '\n'
+            else:
+                help_msg += cmd + '\t' + usage
+            x += 1
         return help_msg
     def process_cmd(self, cmd, username, passwd):
         if self.auth.auth(username, passwd):
