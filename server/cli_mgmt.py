f8045ade0bbcc209f004ac0f24dd33442bb3bac4
LukeG007 <luke@gilsinger.net>
2022-06-26 15:43:29 -0400

    Start on permissions

--- server/cli_mgmt.py
+++ server/cli_mgmt.py
@@ -5,20 +5,28 @@ class CLI:
     def __init__(self):
         self.auth = auth_cli.AuthenticationManagement()
         self.cmd_callbacks = {
-            'help': {'usage': 'Display Command Details', 'callback': self.help},
-            
+            'help': {'usage': 'Display CMD', 'callback': self.help, 'permission_required': 0}
         }
     def help(self):
-        help_msg = 'Command\tDescription\n'
+        help_msg = 'Command\tDescription\tPermission Required\n'
         x = 1
         for cmd in self.cmd_callbacks:
             usage = self.cmd_callbacks[cmd]['usage']
             if not x == len(self.cmd_callbacks):
-                help_msg += cmd + '\t' + usage + '\n'
+                help_msg += cmd + '\t' + usage + '\t' + self.parse_permission_int(self.cmd_callbacks[cmd]['permission_required']) + '\n'
             else:
-                help_msg += cmd + '\t' + usage
+                help_msg += cmd + '\t' + usage + '\t' + self.parse_permission_int(self.cmd_callbacks[cmd]['permission_required'])
             x += 1
         return help_msg
+    def parse_permission_int(self, permission):
+        if permission == 0:
+            return 'Everyone'
+        if permission == 1:
+            return 'Mod'
+        if permission == 2:
+            return 'Admin'
+        if permission == 3:
+            return 'Owner'
     def process_cmd(self, cmd, username, passwd):
         if self.auth.auth(username, passwd):
             return self.cmd_callbacks[cmd]['callback']()
