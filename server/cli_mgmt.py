e9252d3edaa1390317f0c69d62d4ea1032fad4b9
LukeG007 <luke@gilsinger.net>
2022-06-27 00:24:41 -0400

    Add usage to help/invalid cmd msg

--- server/cli_mgmt.py
+++ server/cli_mgmt.py
@@ -5,8 +5,8 @@ class CLI:
     def __init__(self, extra_cmds=None):
         self.auth = auth_cli.AuthenticationManagement()
         self.cmd_callbacks = {
-            'help': {'usage': 'Display CMDs', 'callback': self.help, 'permission_required': 0, 'expected_arg_amount': 1},
-            'user_details': {'usage': 'Get user info', 'callback': self.user_details, 'permission_required': 0, 'expected_arg_amount': 2}
+            'help': {'usage': 'help', 'desc': 'Display CMDs', 'callback': self.help, 'permission_required': 0, 'expected_arg_amount': 1},
+            'user_details': {'usage': 'user_details <user>', 'desc': 'Get user info', 'callback': self.user_details, 'permission_required': 0, 'expected_arg_amount': 2}
         }
         if not extra_cmds is None:
             for cmd in extra_cmds:
@@ -20,14 +20,15 @@ class CLI:
             return 'No user with that username was found'
         return 'Username: {}\nPermissions: {} ({})'.format(user, permissions, self.parse_permission_int(permissions))
     def help(self, args):
-        help_msg = 'Command\tDescription\tPermission Required\n'
+        help_msg = 'Command\tUsage\tDescription\tPermission Required\n'
         x = 1
         for cmd in self.cmd_callbacks:
             usage = self.cmd_callbacks[cmd]['usage']
+            desc = self.cmd_callbacks[cmd]['desc']
             if not x == len(self.cmd_callbacks):
-                help_msg += cmd + '\t' + usage + '\t' + self.parse_permission_int(self.cmd_callbacks[cmd]['permission_required']) + '\n'
+                help_msg += cmd + '\t' + usage + '\t' + desc + '\t' + self.parse_permission_int(self.cmd_callbacks[cmd]['permission_required']) + '\n'
             else:
-                help_msg += cmd + '\t' + usage + '\t' + self.parse_permission_int(self.cmd_callbacks[cmd]['permission_required'])
+                help_msg += cmd + '\t' + usage + '\t' + desc + '\t' + self.parse_permission_int(self.cmd_callbacks[cmd]['permission_required'])
             x += 1
         return help_msg
     def parse_permission_int(self, permission):
@@ -48,7 +49,7 @@ class CLI:
         if not args[0] in self.cmd_callbacks:
             return 'Command not found'
         if not self.validate_cmd(args, self.cmd_callbacks[args[0]]['expected_arg_amount']):
-            return 'Invalid Command'
+            return 'Usage: ' + self.cmd_callbacks[args[0]]['usage']
         if auth and self.cmd_callbacks[args[0]]['permission_required'] <= permissions:
             return self.cmd_callbacks[args[0]]['callback'](args)
         else:
