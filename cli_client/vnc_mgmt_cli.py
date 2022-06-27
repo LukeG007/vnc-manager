a15080924bb9d0aafeb07ab722bc81afba81df94
LukeG007 <luke@gilsinger.net>
2022-06-27 00:35:14 -0400

    Add server_ip.conf to cli_client

--- cli_client/vnc_mgmt_cli.py
+++ cli_client/vnc_mgmt_cli.py
@@ -3,7 +3,10 @@ import getpass
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print('Connecting to server...')
-s.connect(('192.168.3.199', 4582))
+server_ip_f = open('server_ip.conf')
+server_ip = server_ip_f.read()
+server_ip_f.close()
+s.connect((server_ip, 4582))
 username = input('Username: ').encode()
 passwd = getpass.getpass().encode()
 s.send(username + b':' + passwd)
