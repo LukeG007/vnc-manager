625ad34662b9b1095952457a126a165151d02382
LukeG007 <luke@gilsinger.net>
2022-06-26 23:58:12 -0400

    Add server for CLI

--- server/server.py
+++ server/server.py
@@ -1,6 +1,9 @@
 from flask import Flask, request
 import uuid
 import json
+import threading
+import cli_session_mgmt
+import socket
 
 app = Flask(__name__)
 client_list = {}
@@ -17,5 +20,38 @@ def list_clients():
     clients = json.dumps(client_list)
     return clients
 
+def manage_cli_session(conn):
+    try:
+        authentication = conn.recv(1024).decode('utf-8').split(':')
+    except ConnectionResetError:
+        print('Connection to client lost, closing thread')
+    else:
+        if not authentication == '':
+            session = cli_session_mgmt.CLISessionManagement(authentication[0], authentication[1])
+            while True:
+                try:
+                    cmd = conn.recv(1024).decode('utf-8')
+                    if cmd == 'exit':
+                        conn.close()
+                        break
+                    else:
+                        resp = session.execute(cmd)
+                        conn.send(resp.encode())
+                except ConnectionResetError:
+                    print('Connection to client lost, closing thread')
+                    break
+        else:
+            print('Connection to client lost, closing thread')
+
+def start_cli_mgmt():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('0.0.0.0', 4582))
+    s.listen()
+    while True:
+        print('Listening for CLI connections')
+        conn, addr = s.accept()
+        threading.Thread(target=manage_cli_session, args=[conn]).start()
+
 if __name__ == '__main__':
+    threading.Thread(target=start_cli_mgmt).start()
     app.run('0.0.0.0', port=4583)
