42fd633d7a9cc86495599e074ce0631c395989ff
LukeG007 <luke@gilsinger.net>
2022-07-07 01:03:03 -0400

    Fix expected_arg_amount typo

--- server/cli_session_mgmt.py
+++ server/cli_session_mgmt.py
@@ -4,7 +4,7 @@ class CLISessionManagement:
     def __init__(self, username, password, vnc_sys):
         self.cli = cli_mgmt.CLI(vnc_sys, extra_cmds={
             'whoami': {'usage': 'whoami', 'desc': 'Check current user', 'callback': self.whoami, 'permission_required': 0, 'expected_arg_amount': 1},
-            'create_vnc': {'usage': 'create_vnc <port>', 'desc': 'Create vnc server', 'callback': self.create_vnc, 'permission_required': 2, 'expected_arg_amount': 3},
+            'create_vnc': {'usage': 'create_vnc <port>', 'desc': 'Create vnc server', 'callback': self.create_vnc, 'permission_required': 2, 'expected_arg_amount': 2},
             'start_vnc': {'usage': 'start_vnc <port>', 'desc': 'Start vnc server', 'callback': self.start_vnc, 'permission_required': 1, 'expected_arg_amount': 2}
         })
         self.username = username
