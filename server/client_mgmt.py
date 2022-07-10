22a6bb647cb4ef3f41d73e2f3f12a3684ce9c964
LukeG007 <luke@gilsinger.net>
2022-07-10 01:24:51 -0400

    Make /add/<host> route not return None as resp

--- server/client_mgmt.py
+++ server/client_mgmt.py
@@ -7,6 +7,7 @@ class ClientManagement:
     def add_client(self, hostname, ip):
         mgmt_id = str(uuid.uuid4()).split('-')[0]
         self.client_list[mgmt_id] = {'hostname': hostname, 'ip': ip, 'vnc_server_amount': 0}
+        return 'ok'
     def send_start_cmd(self, mgmt_id, authentication, port):
         server = self.client_list[mgmt_id]
         requests.post('http://{}:4584/start'.format(server['ip']), json={'auth': authentication, 'port': port})
