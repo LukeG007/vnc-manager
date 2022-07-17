e1becb69328f3c37f8eda2438abae843cdd0ea8e
LukeG007 <luke@gilsinger.net>
2022-07-17 00:36:57 -0400

    Add command for stopping VNC servers

--- cli_session_mgmt.py
+++ cli_session_mgmt.py
@@ -5,7 +5,8 @@ class CLISessionManagement:
         extra_cmds={
             'whoami': {'usage': 'whoami', 'desc': 'Check current user', 'callback': self.whoami, 'permission_required': 0, 'expected_arg_amount': 1},
             'create_vnc': {'usage': 'create_vnc <port>', 'desc': 'Create vnc server', 'callback': self.create_vnc, 'permission_required': 2, 'expected_arg_amount': 2},
-            'start_vnc': {'usage': 'start_vnc <port>', 'desc': 'Start vnc server', 'callback': self.start_vnc, 'permission_required': 1, 'expected_arg_amount': 2}
+            'start_vnc': {'usage': 'start_vnc <port>', 'desc': 'Start vnc server', 'callback': self.start_vnc, 'permission_required': 1, 'expected_arg_amount': 2},
+            'stop_vnc': {'usage': 'stop_vnc <port>', 'desc': 'Stop vnc server', 'callback': self.stop_vnc, 'permission_required': 1, 'expected_arg_amount': 2}
         }
         self.cli = cli_mgmt.CLI(vnc_sys, extra_cmds=extra_cmds)
         self.username = username
@@ -25,6 +26,14 @@ class CLISessionManagement:
     def start_vnc(self, args):
         port = args[1]
         resp = self.vnc_sys.start_vnc_server(port, self.username, self.password)
+        if resp[1] == 1:
+            resp = 'ERROR: '+resp[0]
+        else:
+            resp = resp[0]
+        return resp
+    def stop_vnc(self, args):
+        port = args[1]
+        resp = self.vnc_sys.stop_vnc_server(port, self.username, self.password)
         if resp[1] == 1:
             resp = 'ERROR: '+resp[0]
         else:
