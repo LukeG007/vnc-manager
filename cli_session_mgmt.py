cbcfafb7ea2c486392847534eacc5a34af90bb90
LukeG007 <luke@gilsinger.net>
2022-07-17 21:24:54 -0400

    Add command for deleting VNC servers

--- cli_session_mgmt.py
+++ cli_session_mgmt.py
@@ -5,6 +5,7 @@ class CLISessionManagement:
         extra_cmds={
             'whoami': {'usage': 'whoami', 'desc': 'Check current user', 'callback': self.whoami, 'permission_required': 0, 'expected_arg_amount': 1},
             'create_vnc': {'usage': 'create_vnc <port>', 'desc': 'Create vnc server', 'callback': self.create_vnc, 'permission_required': 2, 'expected_arg_amount': 2},
+            'delete_vnc': {'usage': 'delete_vnc <port>', 'desc': 'Delete vnc server', 'callback': self.delete_vnc, 'permission_required': 2, 'expected_arg_amount': 2},
             'start_vnc': {'usage': 'start_vnc <port>', 'desc': 'Start vnc server', 'callback': self.start_vnc, 'permission_required': 1, 'expected_arg_amount': 2},
             'stop_vnc': {'usage': 'stop_vnc <port>', 'desc': 'Stop vnc server', 'callback': self.stop_vnc, 'permission_required': 1, 'expected_arg_amount': 2}
         }
@@ -18,6 +19,14 @@ class CLISessionManagement:
     def create_vnc(self, args):
         port = args[1]
         resp = self.vnc_sys.create_vnc_server(self.username, self.password, port)
+        if resp[1] == 1:
+            resp = 'ERROR: '+resp[0]
+        else:
+            resp = resp[0]
+        return resp
+    def delete_vnc(self, args):
+        port = args[1]
+        resp = self.vnc_sys.delete_vnc_server(self.username, self.password, port)
         if resp[1] == 1:
             resp = 'ERROR: '+resp[0]
         else:
